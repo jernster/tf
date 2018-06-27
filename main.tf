09b67f02cf9bccd6ebda9c13e2ec5ee77ae5b98c
Jon Ernster <jon.ernster@gmail.com>
2018-06-26 23:31:51 -0600

    Add auto scaling groups and elastic load balancer

--- main.tf
+++ main.tf
@@ -2,27 +2,19 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "ssb_test" {
-  #ami                    = "ami-e251209a" # Amazon Linux 2018.03.0
-  ami                    = "ami-db710fa3"                        #Ubuntu 16.04 
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+resource "aws_launch_configuration" "ssb_test" {
+  image_id        = "ami-db710fa3"                        #Ubuntu 16.04 
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
-  #user_data = <<-EOF
-  #            #!/bin/sh
-  #            yum install httpd
-  #            chkconfig httpd on
-  #            echo "Hello, Apache" > /var/www/html/index.html
-  #            service httpd start
-  #            EOF
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 
-  tags {
-    Name = "ssb-terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -42,6 +34,64 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.ssb_test.public_ip}"
+output "elb_dns_name" {
+  value = "${aws_elb.elb_example.dns_name}"
+}
+
+resource "aws_autoscaling_group" "asg_example" {
+  launch_configuration = "${aws_launch_configuration.ssb_test.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.elb_example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tags {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_elb" "elb_example" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
